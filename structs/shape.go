996bb5755966568a24189418e0a819791a5c68c8
wukai <wuk.jlu@outlook.com>
2022-02-10 19:42:37 +0800

    重构测试，更简洁明了扩展性强

--- structs/shape.go
+++ structs/shape.go
@@ -5,6 +5,10 @@ import "math"
 type Shape interface {
 	Area() float64
 }
+type Triangle struct {
+	Base   float64
+	Height float64
+}
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -22,4 +26,7 @@ func (r Rectangle) Area() float64 {
 }
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+func (c Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
 }
